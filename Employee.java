db66c5cafdce616174c1e9ea08f160f532f38323
Shaikyasee <134361822+Shaikyasee@users.noreply.github.com>
2023-05-26 12:15:15 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,28 @@
+package model.dadu;
+
+public class Employee {
+	
+	private int empno;
+	private String name;
+	
+	
+	public int getEmpno() {
+		return empno;
+	}
+	public void setEmpno(int empno) {
+		this.empno = empno;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Override
+	public String toString() {
+		return "Employee [empno=" + empno + ", name=" + name + "]";
+	}
+	 
+	
+
+}
